0031358c556ba8913803ad623c1fe0bdfbb2db43
NichtJens <nichtjens@gmail.com>
2020-06-11 15:24:03 +0200

    added files for packaging: setuptools, travis, conda

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sfdata",
+    version="0.0.1",
+    url="https://github.com/paulscherrerinstitute/sf_datafiles",
+    description="SwissFEL Data Files",
+    author="Paul Scherrer Institute",
+    packages=find_packages()
+)
+
+
