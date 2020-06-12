01aa00e333d7a392e108c326acb09a9585702ed0
NichtJens <nichtjens@gmail.com>
2020-06-12 11:00:14 +0200

    added a simple way to get h5 to boolean index along the first axis (like numpy does)

--- sfdata/utils.py
+++ sfdata/utils.py
@@ -1,3 +1,19 @@
+import numpy as np
+
+
+def h5_boolean_indexing(ds, indices):
+    """
+    hdf5 does not support boolean indexing on the first axis for n-dim. datasets:
+    https://github.com/h5py/h5py/issues/626
+    This function calculates the positions of the Trues in the boolean indices 
+    and uses those as coordinates within the dataset.
+    """
+    if ds.ndim == 1:
+        return ds[indices]
+    assert indices.ndim == 1, "indices needs to be 1D"
+    coords = np.nonzero(indices)[0].tolist()
+    return ds[coords]
+
 
 def typename(obj):
     return type(obj).__name__
