93ada78d85f45676ed3a9b17eb3bd8c73eb6e4fd
NichtJens <nichtjens@gmail.com>
2020-06-23 12:52:24 +0200

    added cprint()

---
+++ sfdata/cprint.py
@@ -0,0 +1,38 @@
+from colorama import Fore
+
+
+COLORS = {
+    "black":   Fore.BLACK,
+    "red":     Fore.RED,
+    "green":   Fore.GREEN,
+    "yellow":  Fore.YELLOW,
+    "blue":    Fore.BLUE,
+    "magenta": Fore.MAGENTA,
+    "cyan":    Fore.CYAN,
+    "white":   Fore.WHITE,
+    "reset":   Fore.RESET,
+    None:      Fore.RESET
+}
+
+
+def cprint(*objects, color=None, sep=" ", **kwargs):
+    color = get_color(color)
+    text = flatten_strings(objects, sep)
+    return _print(color, text, sep, kwargs)
+
+def get_color(color):
+    try:
+        return COLORS[color]
+    except KeyError as e:
+        color = repr(color)
+        allowed = tuple(COLORS.keys())
+        raise ValueError(f"{color} not from {allowed}") from e
+
+def flatten_strings(objects, sep):
+    return sep.join(str(i) for i in objects)
+
+def _print(color, text, sep, kwargs):
+    return print(color + text + Fore.RESET, sep=sep, **kwargs)
+
+
+
