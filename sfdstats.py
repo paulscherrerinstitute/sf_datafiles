6bc879e76c740365bf895362ab31b17cd59bb526
NichtJens <nichtjens@gmail.com>
2020-06-23 14:09:46 +0200

    added a CLI script for displaying statistics

---
+++ sfdstats.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import argparse
+
+parser = argparse.ArgumentParser(description="Print statistics for SwissFEL data files")
+
+parser.add_argument("filenames", type=str, nargs="+", help="names of files to read, accepts wildcards")
+parser.add_argument("-c", "--complete", action="store_true", help="also show channels which have the complete set of pulse IDs")
+
+clargs = parser.parse_args()
+
+
+from sfdata import SFDataFiles
+
+with SFDataFiles(*clargs.filenames) as data:
+    data.print_stats(show_complete=clargs.complete)
+
+
+
