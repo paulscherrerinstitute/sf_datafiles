8da158ba11885706342a2581dd03d06b9da64574
NichtJens <nichtjens@gmail.com>
2020-06-24 09:57:53 +0200

    wrapped script in main() to satisfy conda entry_points

--- sfdstats.py
+++ sfdstats.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 
-import argparse
+def main():
+    import argparse
 
-parser = argparse.ArgumentParser(description="Print statistics for SwissFEL data files")
+    parser = argparse.ArgumentParser(description="Print statistics for SwissFEL data files")
 
-parser.add_argument("filenames", type=str, nargs="+", help="names of files to read, accepts wildcards")
-parser.add_argument("-c", "--complete", action="store_true", help="also show channels which have the complete set of pulse IDs")
+    parser.add_argument("filenames", type=str, nargs="+", help="names of files to read, accepts wildcards")
+    parser.add_argument("-c", "--complete", action="store_true", help="also show channels which have the complete set of pulse IDs")
 
-clargs = parser.parse_args()
+    clargs = parser.parse_args()
 
 
-from sfdata import SFDataFiles
+    from sfdata import SFDataFiles
 
-with SFDataFiles(*clargs.filenames) as data:
-    data.print_stats(show_complete=clargs.complete)
+    with SFDataFiles(*clargs.filenames) as data:
+        data.print_stats(show_complete=clargs.complete)
+
+
+
+if __name__ == '__main__':
+    main()
 
 
 
