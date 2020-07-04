3886ee5c429725e6a9890988b8e5980a8ce4a5c8
NichtJens <nichtjens@gmail.com>
2020-07-04 19:42:16 +0200

    added some string(s) printing utils

--- sfdata/utils.py
+++ sfdata/utils.py
@@ -72,4 +72,12 @@ def print_line(length=80, char="-"):
     print("\n" + char*length + "\n")
 
 
+def printable_string_sequence(strings):
+    strings = (nice_string_repr(s) for s in strings)
+    return ", ".join(strings)
+
+def nice_string_repr(string):
+    return repr(string).replace("'", '"')
+
+
 
