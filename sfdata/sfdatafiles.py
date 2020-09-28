c668296174828235b9d3939151ed5bc7399b56e2
NichtJens <nichtjens@gmail.com>
2020-09-28 16:10:15 +0200

    adjusted logic of loading multiple files: continue with file-list iteration if the loading of a file fails, only error if no file was opened at all

--- sfdata/sfdatafiles.py
+++ sfdata/sfdatafiles.py
@@ -10,13 +10,25 @@ class SFDataFiles(FileContext, SFData):
 
     def __init__(self, *patterns):
         self.fnames = fnames = explode_filenames(patterns)
-        if not fnames:
+
+        self.files = files = []
+        for fn in fnames:
+            try:
+                f = SFDataFile(fn)
+            except Exception as exc:
+                excname = typename(exc)
+                print(f"Warning: Skipping \"{fn}\" since it caused {excname}: {exc}")
+            else:
+                files.append(f)
+
+        if not files:
             patterns = printable_string_sequence(patterns)
             raise ValueError(f"No matching file for patterns: {patterns}")
-        self.files = [SFDataFile(fn) for fn in fnames]
+
         super().__init__()
-        for f in self.files:
+        for f in files:
             self.update(f)
+
 
     def close(self):
         for f in self.files:
