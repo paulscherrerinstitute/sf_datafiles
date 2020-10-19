a27ba885489ebb17680c0769791fcd8c43b53e7b
NichtJens <nichtjens@gmail.com>
2020-10-19 19:29:04 +0200

    make sure arr is an array (since shape adjustment can only be done on arrays)

--- sfdata/utils/np.py
+++ sfdata/utils/np.py
@@ -1,6 +1,9 @@
+import numpy as np
+
 
 def adjust_shape(arr):
     """transpose 1D column vectors to line vectors"""
+    arr = np.asanyarray(arr)
     if arr.ndim == 2 and arr.shape[1] == 1:
         arr = arr.reshape(-1)
     return arr
