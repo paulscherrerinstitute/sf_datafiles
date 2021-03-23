589bfd01d3025ce68f76802f85bb8649c429a3ea
NichtJens <nichtjens@gmail.com>
2021-03-23 18:08:45 +0100

    Release v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sfdata",
-    version="0.1.0",
+    version="0.1.1",
     url="https://github.com/paulscherrerinstitute/sf_datafiles",
     description="SwissFEL Data Files",
     author="Paul Scherrer Institute",
