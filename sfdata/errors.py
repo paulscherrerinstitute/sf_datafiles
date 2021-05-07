654b80a2694053e9b16e394421b9f95d961cb950
NichtJens <nichtjens@gmail.com>
2021-05-07 11:46:38 +0200

    added exception base class to simplify catching any of the custom exception

--- sfdata/errors.py
+++ sfdata/errors.py
@@ -1,26 +1,30 @@
 
-class NoUsableFileError(Exception):
+class SFDataError(Exception):
+    pass
+
+
+class NoUsableFileError(SFDataError):
 
     def __init__(self):
         msg = "No entry contained a usable file"
         super().__init__(msg)
 
 
-class NoMatchingFileError(Exception):
+class NoMatchingFileError(SFDataError):
 
     def __init__(self, patterns):
         msg = f"No matching file for patterns: {patterns}"
         super().__init__(msg)
 
 
-class NoUsableChannelError(Exception):
+class NoUsableChannelError(SFDataError):
 
     def __init__(self, fname):
         msg = f"No usable channel in: {fname}"
         super().__init__(msg)
 
 
-class DatasetNotInGroupError(Exception):
+class DatasetNotInGroupError(SFDataError):
 
     def __init__(self, name, group):
         msg = f"Cannot get dataset \"{name}\" from: {group}"
