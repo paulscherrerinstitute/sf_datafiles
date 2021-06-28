7616a202ec2c18ba84264edd7903b112e3167b8c
NichtJens <nichtjens@gmail.com>
2021-06-28 13:13:52 +0200

    added no-color toggle

--- sfdstats.py
+++ sfdstats.py
@@ -7,6 +7,7 @@ def main():
 
     parser.add_argument("filenames", type=str, nargs="+", help="names of files to read, accepts wildcards")
     parser.add_argument("-c", "--complete", action="store_true", help="also show channels that have the complete set of pulse IDs")
+    parser.add_argument("-n", "--no-color", action="store_true", help="do not color the output")
 
     clargs = parser.parse_args()
 
@@ -14,7 +15,7 @@ def main():
     from sfdata import SFDataFiles
 
     with SFDataFiles(*clargs.filenames) as data:
-        data.print_stats(show_complete=clargs.complete)
+        data.print_stats(show_complete=clargs.complete, color=not clargs.no_color)
 
 
 
