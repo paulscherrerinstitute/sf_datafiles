181dae3003ef719a9f617b4b7294299f51704f2a
NichtJens <nichtjens@gmail.com>
2022-02-09 16:29:02 +0100

    handle run=sequence

--- sfdata/load.py
+++ sfdata/load.py
@@ -1,14 +1,39 @@
+import collections
+import itertools
 from glob import iglob
 
 
 def make_loader(instrument="*a", pgroup="p*", run="*"):
     def loader_wrapper(instrument=instrument, pgroup=pgroup, run=run, debug=False):
-        return loader(instrument, pgroup, run, debug)
+        return list(load_many(instrument, pgroup, run, debug))
     return loader_wrapper
 
 
 
-def loader(instrument, pgroup, run, debug):
+def load_many(instrument, pgroup, run, debug):
+    for i, p, r in product(instrument, pgroup, run):
+        yield from load_one(i, p, r, debug)
+
+
+
+def product(*vals):
+    vals = ensure_seqs(*vals)
+    return itertools.product(*vals)
+
+def ensure_seqs(*vals):
+    return (ensure_seq(i) for i in vals)
+
+def ensure_seq(val):
+    if isiterable(val):
+        return val
+    return (val,)
+
+def isiterable(val):
+    return isinstance(val, collections.abc.Iterable) and not isinstance(val, str)
+
+
+
+def load_one(instrument, pgroup, run, debug):
     pgroup = harmonize(pgroup, "p", 5)
     run = harmonize(run, "run", 4)
 
@@ -17,7 +42,7 @@ def loader(instrument, pgroup, run, debug):
 
     if debug:
         print(pattern, "-> #runs:", len(res))
-        return pattern
+        return [pattern]
     else:
         return res
 
@@ -66,12 +91,12 @@ if __name__ == "__main__":
     load = partial(load, debug=True)
 
 
-    assert load(instrument="TEST") == "/sf/TEST/data/p*/raw/run*"
-    assert load(pgroup="TEST") == "/sf/*a/data/pTEST/raw/run*"
-    assert load(run="TEST") == "/sf/*a/data/p*/raw/runTEST"
+    assert load(instrument="TEST") == ["/sf/TEST/data/p*/raw/run*"]
+    assert load(pgroup="TEST")     == ["/sf/*a/data/pTEST/raw/run*"]
+    assert load(run="TEST")        == ["/sf/*a/data/p*/raw/runTEST"]
 
 
-    ref = "/sf/*a/data/p*/raw/run*"
+    ref = ["/sf/*a/data/p*/raw/run*"]
 
     assert ref == load()
     assert ref == load(run="*")
@@ -80,7 +105,7 @@ if __name__ == "__main__":
     assert ref == load(pgroup="p*")
 
 
-    ref = "/sf/*a/data/p*/raw/run0001"
+    ref = ["/sf/*a/data/p*/raw/run0001"]
 
     assert ref == load(run=1)
     assert ref == load(run="1")
@@ -89,11 +114,20 @@ if __name__ == "__main__":
     assert ref == load(run="run00001")
 
 
-    ref = "/sf/*a/data/p12345/raw/run*"
+    ref = ["/sf/*a/data/p12345/raw/run*"]
 
     assert ref == load(pgroup=12345)
     assert ref == load(pgroup="12345")
     assert ref == load(pgroup="p12345")
+
+
+    ref = [
+        "/sf/*a/data/p*/raw/run0010",
+        "/sf/*a/data/p*/raw/run0020"
+    ]
+
+    assert ref == load(run=[10, 20])
+    assert ref == load(run=iter(range(10, 20+10, 10)))
 
 
 
