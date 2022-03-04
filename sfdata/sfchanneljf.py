b125e96fc665a32f46d3b5092cec89620d9e7dc7
gac-maloja <maloja@psi.ch>
2022-03-04 19:30:40 +0100

    changed np.where to ndarray.nonzero according to the numpy docs for np.where

--- sfdata/sfchanneljf.py
+++ sfdata/sfchanneljf.py
@@ -25,8 +25,7 @@ class SFChannelJF(SFChannel):
         good = good[:]
         if good.all():
             return
-        good = good.reshape(-1).astype(bool)
-        good = np.where(good)[0]
+        good = good.reshape(-1).nonzero()[0]
         self.valid = good
 
 
