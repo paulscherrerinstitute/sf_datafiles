b2e9be53ed73fc9f972818699550b433730c1219
NichtJens <nichtjens@gmail.com>
2022-10-18 19:02:52 +0200

    sorted list of imports

--- test/test.py
+++ test/test.py
@@ -14,18 +14,18 @@ sys.path.insert(0, (os.path.join(this_dir, "..")))
 
 
 if __name__ == '__main__':
-    from test_sfdata import TestSFData
-    from test_sfdatafile import TestSFDataFile
-    from test_sfdatafiles import TestSFDataFiles
-    from test_sfchannel import TestSFChannel
-    from test_sfchanneljf import TestSFChannelJF
-    from test_sfscaninfo import TestSFScanInfo
-    from test_sfprocfile import TestSFProcFile
     from test_errors import TestErrors
     from test_filecontext import TestFileContext
     from test_ign import TestIgn
-    from test_utils import TestUtils
+    from test_sfchannel import TestSFChannel
+    from test_sfchanneljf import TestSFChannelJF
+    from test_sfdatafile import TestSFDataFile
+    from test_sfdatafiles import TestSFDataFiles
+    from test_sfdata import TestSFData
+    from test_sfprocfile import TestSFProcFile
+    from test_sfscaninfo import TestSFScanInfo
     from test_utils_filestatus import TestUtilsFileStatus
+    from test_utils import TestUtils
 
     import unittest
     unittest.main()
