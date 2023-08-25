7056136546c320d6a7661c14bd1edaf25c91dea0
NichtJens <nichtjens@gmail.com>
2023-08-25 11:46:20 +0200

    added name and substitute to FileDemultiplexer

--- sfdata/utils/fdemux.py
+++ sfdata/utils/fdemux.py
@@ -1,8 +1,22 @@
 
 class FileDemultiplexer(set):
 
+    def __init__(self, *args, name=None, substitute=None):
+        self.name = name
+        self.substitute = substitute
+        super().__init__(*args)
+
+    def __getitem__(self, key):
+        if self.substitute:
+            return self.substitute[key]
+        else:
+            raise ValueError("FileDemultiplexer without substitute is not subscriptable")
+
     def close(self):
         for f in self:
             f.close()
+        if self.substitute:
+            self.substitute.close()
+
 
 
