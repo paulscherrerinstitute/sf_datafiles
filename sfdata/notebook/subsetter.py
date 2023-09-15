7c51536ec3edad3ea63b0c0351b35f216914eb55
NichtJens <nichtjens@gmail.com>
2023-09-15 12:23:12 +0200

    added some info the the lines

--- sfdata/notebook/subsetter.py
+++ sfdata/notebook/subsetter.py
@@ -1,3 +1,4 @@
+from sfdata.utils import dip, percentage_missing
 from .widgets import Selector
 
 
@@ -5,12 +6,34 @@ class SubSetter(Selector):
 
     def __init__(self, sfd):
         self.sfd = sfd
-        names = [[n] for n in sfd.names]
-        super().__init__(names, ["Name"])
+        n_shared_pids, n_all_pids = get_general(sfd)
+        names = [get_line(n, sfd,n_shared_pids, n_all_pids) for n in sfd.names]
+        super().__init__(names, ["Name", "#valid", "#shared", "#total", "%"])
 
     def get(self):
         selection = self.get_filtered_list()
         return self.sfd[selection]
+
+
+
+
+
+def get_general(sfd):
+    shared_pids = sfd.pids
+    all_pids    = sfd.all_pids
+
+    n_shared_pids = len(shared_pids)
+    n_all_pids    = len(all_pids)
+
+    return n_shared_pids, n_all_pids
+
+
+def get_line(name, sfd, n_shared_pids, n_all_pids):
+    ch = sfd[name]
+    n_valid = ch.nvalid
+    perc = percentage_missing(n_valid, n_all_pids)
+    blocks = dip(perc)
+    return (name, n_valid, n_shared_pids, n_all_pids, blocks)
 
 
 
