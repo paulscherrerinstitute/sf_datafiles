22c9026640bd0c4f94c5d601a0a9a2f640071d9b
NichtJens <nichtjens@gmail.com>
2025-05-26 22:15:28 +0200

    handle all true correctly

--- sfdata/plot_missing.py
+++ sfdata/plot_missing.py
@@ -36,7 +36,7 @@ def plot_bools(data, color_true="tab:green", color_false="tab:red"):
 
     for i, (ax, (lbl, arr)) in enumerate(zip(axes, data.items())):
         img = arr[np.newaxis, :]
-        ax.imshow(img, aspect="auto", interpolation="nearest", extent=extent, cmap=cmap)
+        ax.imshow(img, aspect="auto", interpolation="nearest", extent=extent, cmap=cmap, vmin=0, vmax=1)
         ax.set_ylabel(lbl, rotation=0, ha="right", va="center")
         ax.set_yticks([])
         ax.tick_params(labelbottom=False)
@@ -58,6 +58,8 @@ if __name__ == "__main__":
         "A" * 10: np.random.rand(N) > 0.5,
         "B" * 5:  np.random.rand(N) > 0.7,
         "C":      np.random.rand(N) > 0.3,
+        "Y":      np.ones([N], dtype=bool),
+        "N":      np.zeros([N], dtype=bool)
     }
 
     plot_bools(data)
